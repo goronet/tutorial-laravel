417df67d33c002b424514f30e64e117482969ae6
Goronet <goronet@gmail.com>
2024-01-04 00:59:19 +0100

    Rutas con prefix y group

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,58 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+
+});
+
+// CATEGORÍAS
+Route::prefix('categorias')->group(function () {
+    Route::get('/', function () {
+        $categorias = [
+            'Fideos',
+            'Verduras',
+            'Arroz'
+        ];
+
+        foreach ($categorias as $categoria) {
+            echo $categoria . '<br>';
+        }
+    });
+
+    Route::get('{nombreCategoria}', function (string $nombreCategoria) {
+        echo 'productos de ' . $nombreCategoria;
+    });
+});
+
+Route::get('productos/{categoria?}', function (?string $categoria = null) {
+    $categorias = [
+        'Fideos' => [
+            'Moñitos',
+            'Fideos largos',
+            'Cabello de angel'
+        ],
+        'Verduras' => [
+            'Tomate',
+            'Lechuga',
+            'Cebolla'
+        ]
+    ];
+
+    // si el usuario no ingresa categoría
+    if (is_null($categoria)) {
+        // muestro todos los productos
+        foreach ($categorias as $categoriaArray) {
+            foreach ($categoriaArray as $producto) {
+                echo $producto . '<br>';
+            }
+        }
+    } else {
+        // checkeo si el nombre de la categoria existe en el array
+        if (array_key_exists($categoria, $categorias)) {
+            foreach ($categorias[$categoria] as $producto) {
+                echo $producto . '<br>';
+            }
+        }
+    }
 });
